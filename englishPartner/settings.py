60c00fd757471b06730c1131ee07f59e4ea83bfe
kavyashree2023 <kavyashree082023@gmail.com>
2023-10-31 15:40:15 +0530

    sdfgv

--- englishPartner/settings.py
+++ englishPartner/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,12 +75,17 @@ WSGI_APPLICATION = 'englishPartner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+database_url="postgres://kavya:eF5YgfAxuLKYBQ6lv8k5hFsuXWqvrSjR@dpg-cl0c0jas1bgc73afvqug-a.oregon-postgres.render.com/test123_k8hy"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.parse("postgres://kavya:eF5YgfAxuLKYBQ6lv8k5hFsuXWqvrSjR@dpg-cl0c0jas1bgc73afvqug-a.oregon-postgres.render.com/test123_k8hy")
 }
+
 
 
 # Password validation
